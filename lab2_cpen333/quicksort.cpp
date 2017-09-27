db22acb5c78703e74ce301ea7b85cb3ae3e85ad8
bd05 <berdao.09@gmail.com>
2017-09-26 18:39:36 -0700

    multithread quicksort works

--- lab2_cpen333/quicksort.cpp
+++ lab2_cpen333/quicksort.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
 #include <thread>
 #include <vector>
+#include <Windows.h>
+
 
 void swap(int* a, int* b){
 	int temp = *a;
@@ -37,6 +40,18 @@ void quicksort(std::vector<int>& data, int low, int high) {
 // sorts elements low through high (inclusive) using multiple threads
 void parallel_quicksort(std::vector<int>& data, int low, int high) {
 	// your code here
+	if (low < high){
+		int partitionIndex = partition(data, low, high);
+		std::thread left(parallel_quicksort, std::ref(data), low, partitionIndex - 1);
+		std::thread right(parallel_quicksort, std::ref(data), partitionIndex + 1, high);
+		left.join();
+		right.join();
+	}
+}
+
+void task1(std::string msg)
+{
+	std::cout << "task1 says: " << msg;
 }
 
 int main() {
@@ -51,18 +66,27 @@ int main() {
 		v1[i] = rand();
 		std::cout << v1[i] << " ";
 	}
+	std::cout << std::endl;
 
 	std::vector<int> v2 = v1;  // copy all contents
 
 	// sort v1 using sequential algorithm
 	std::cout << "after sorting, v1: " << std::endl;
 	quicksort(v1, 0, v1.size() - 1);
+	//print to test
 	for (int i = 0; i < VECTOR_SIZE; ++i){
-		std::cout << v1[i] << ", ";
+		std::cout << v1[i] << " ";
 	}
+	std::cout << std::endl;
 
 	// sort v2 using parallel algorithm
 	parallel_quicksort(v2, 0, v2.size() - 1);
+	//print to test
+	std::cout << "after sorting, v2: " << std::endl;
+	for (int i = 0; i < VECTOR_SIZE; ++i){
+		std::cout << v2[i] << " ";
+	}
+	std::cout << std::endl;
 
 	std::cout << std::endl << "end" << std::endl;
 	return 0;
